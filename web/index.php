bd2534b70bfcee6f57b3f5217a7062812d5eec5c
ValentinLipatov <valentin-ltd@mail.ru>
2018-02-04 22:58:41 +0300

    Update index.php

--- web/index.php
+++ web/index.php
@@ -2,11 +2,11 @@
     header("Content-Type: text/html; charset=UTF-8");
     //include("autorization.php");
 
-    if ($aut) 
-    {
-        header('Location: action.php');
-        exit;
-    }
+   // if ($aut) 
+   // {
+    //    header('Location: action.php');
+    //    exit;
+   //}
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
