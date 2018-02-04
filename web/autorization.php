ca44d4f7b6ada305c1285320be83d5d666762c7e
ValentinLipatov <valentin-ltd@mail.ru>
2018-02-04 22:25:42 +0300

    Add files via upload

--- web/autorization.php
+++ web/autorization.php
@@ -13,6 +13,23 @@
 	password = 9df5fc487653ddec6e25d68ccea35d151387bcfe2c64229c5c350f431f3c9781
     ")or die('Could not connect: ' . pg_last_error());
        
+    $query = "CREATE TABLE users (
+    id SERIAL,
+    name TEXT NOT NULL,
+    log TEXT NOT NULL,
+    pas TEXT NOT NULL,
+    sum double precision NOT NULL)";
+    $result = pg_query($query) or die(pg_last_error());
+
+    $query = "CREATE TABLE transaction (
+    id SERIAL,
+    user_id INT NOT NULL,
+    datetime TEXT NOT NULL,
+    type TEXT NOT NULL,
+    sum double precision NOT NULL,
+    status TEXT NOT NULL)";
+    $result = pg_query($query) or die(pg_last_error());
+            
     $error = "";
     $succs = "";  
     
