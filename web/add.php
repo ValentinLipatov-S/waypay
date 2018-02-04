4a865ccedafe3eb206da980873308b91a4908f63
ValentinLipatov <valentin-ltd@mail.ru>
2018-02-04 22:34:43 +0300

    Add files via upload

--- web/add.php
+++ web/add.php
@@ -1,4 +1,4 @@
-<?
+<?php
     header("Content-Type: text/html; charset=UTF-8");
     if(isset($_GET['add']))
     {
--- web/create.php
+++ web/create.php
@@ -1,4 +1,4 @@
-<?
+<?php
     header("Content-Type: text/html; charset=UTF-8");
     if(isset($_GET['add']))
     {
@@ -42,7 +42,7 @@
         pg_close($dbconn);  
     }
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
     <meta http-equiv="X-UA-Compatible" content="IE=EmulateIE10">
