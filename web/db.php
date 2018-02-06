db4b25d86290c7c7daf210f4932acbbd908581fa
ValentinLipatov <valentin-ltd@mail.ru>
2018-02-06 18:20:13 +0300

    Add files via upload

--- web/db.php
+++ web/db.php
@@ -1,11 +1,6 @@
 ﻿<?php
 	header("Content-Type: text/html; charset=UTF-8");
-	$dbconn = pg_connect("
-	host     = ec2-54-217-245-66.eu-west-1.compute.amazonaws.com
-	dbname   = d1moulb9o83itl
-	user     = cfxbiqrrylhabo
-	password = 9df5fc487653ddec6e25d68ccea35d151387bcfe2c64229c5c350f431f3c9781
-	") or die('Could not connect: ' . pg_last_error());
+	$dbconn = pg_connect("host=ec2-54-217-245-66.eu-west-1.compute.amazonaws.com dbname=d1moulb9o83itl user=cfxbiqrrylhabo port=5432 password=9df5fc487653ddec6e25d68ccea35d151387bcfe2c64229c5c350f431f3c9781") or die('Could not connect: ' . pg_last_error());
 	if(isset($_GET('create')))
 	{
 		$query = "CREATE TABLE users ( id SERIAL, name TEXT NOT NULL, log TEXT NOT NULL, pas TEXT NOT NULL, sum double precision NOT NULL)";	
