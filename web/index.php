29cbe659455ec0d2e0b16af702810fe65b46c38e
ValentinLipatov <valentin-ltd@mail.ru>
2018-02-08 22:57:17 +0300

    Add files via upload

--- web/index.php
+++ web/index.php
@@ -141,8 +141,8 @@
 
     <div class="wrapper">
 
-        <header>
-            <div class="autor lodingblock">
+        <header class = "loadingbar">
+            <div class="autor">
                 <div class="user_container flex_container_row_stretch">
                     <div style="width: 75px; height: 75px; background: url('<?php echo $_SESSION['user_info']['photo_big']; ?>'); background-size: cover; border-radius: 50%;">
                     </div>
