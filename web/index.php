d34237cb23d02c6f1b774564a49e4f4fd4e948b1
ValentinLipatov <valentin-ltd@mail.ru>
2018-02-08 20:32:18 +0300

    Add files via upload

--- web/index.php
+++ web/index.php
@@ -1,4 +1,60 @@
+<?php 
+if (!isset($_SESSION['user_info']))
+{
+    $client_id = '6361837'; // ID РїСЂРёР»РѕР¶РµРЅРёСЏ
+    $client_secret = '9pohMcwaxBYL02WMTR3M'; // Р—Р°С‰РёС‰С‘РЅРЅС‹Р№ РєР»СЋС‡
+    $redirect_uri = 'https://waypay.herokuapp.com/'; // РђРґСЂРµСЃ СЃР°Р№С‚Р°
 
+    $url = 'http://oauth.vk.com/authorize';
+
+    $params = array(
+    'client_id'     => $client_id,
+    'redirect_uri'  => $redirect_uri,
+    'response_type' => 'code'
+    );
+
+    echo '<a href = "' . $url . '?' . urldecode(http_build_query($params))) . '"</a>"';
+    if (isset($_GET['code'])) {
+        
+    $params = array(
+        'client_id' => $client_id,
+        'client_secret' => $client_secret,
+        'code' => $_GET['code'],
+        'redirect_uri' => $redirect_uri
+    );
+
+    $token = json_decode(file_get_contents('https://oauth.vk.com/access_token' . '?' . urldecode(http_build_query($params))), true);
+
+    if (isset($token['access_token'])) 
+    {
+        $params = array(
+            'user_ids'         => $token['user_id'],
+            'fields'       => 'id,first_name,last_name,screen_name,sex,bdate,photo_big',
+            'access_token' => $token['access_token'],
+            'v' => '5.71',
+            'name_case' => 'Nom'
+        );
+
+        $userInfo = json_decode(file_get_contents('https://api.vk.com/method/users.get' . '?' . urldecode(http_build_query($params))), true);
+        print_r($userInfo);
+
+        if (isset($userInfo['response'][0]['id'])) {
+            $userInfo = $userInfo['response'][0];
+
+            session_start();
+            $_SESSION['user_info'] = $userInfo;
+        }
+    }
+}
+else 
+{
+    if(isset($_POST['logout']))
+    {
+        unset($_SESSION['user_info']);
+        session_destroy();
+    }
+}
+?>
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="ru">
 <head>
@@ -15,11 +71,11 @@
         <header>
             <div class="autor">
                 <div class="user_container flex_container_row_stretch">
-                    <div style="width: 75px; height: 75px; background: url('<? echo $_SESSION['user_info']['photo_big']?>'); background-size: cover; border-radius: 50%;"></div>
+                    <div style="width: 75px; height: 75px; background: url('<?php echo $_SESSION['user_info']['photo_big']; ?>'); background-size: cover; border-radius: 50%;"></div>
                         <div style="flex: 1 1; padding: 0px 0px 0px 10px;">
                             <div>
-                                <text class="name" style="display: flex;"><? echo $_SESSION['user_info']['first_name']?>&nbsp;<? echo $_SESSION['user_info']['last_name']?></text>
-                                <text style="display: flex; font-size: 11px;"><? echo $_SESSION['user_info']['id']?></text>
+                                <text class="name" style="display: flex;"><?php echo $_SESSION['user_info']['first_name'];?>&nbsp;<?php echo $_SESSION['user_info']['last_name'];?></text>
+                                <text style="display: flex; font-size: 11px;"><?php echo $_SESSION['user_info']['id'];?></text>
                             </div>
                         </div>
                     </div>
