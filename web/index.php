b67fd27fa07c0e299f795634ce49d8d788843fbf
ValentinLipatov <valentin-ltd@mail.ru>
2018-02-08 20:48:02 +0300

    Add files via upload

--- web/index.php
+++ web/index.php
@@ -6,45 +6,49 @@
         $redirect_uri = 'https://waypay.herokuapp.com/';
 
         $url = 'http://oauth.vk.com/authorize';
-
-        $params = array(
-        'client_id'     => $client_id,
-        'redirect_uri'  => $redirect_uri,
-        'response_type' => 'code'
-        );
-
-        header("location: " . $url . '?' . urldecode(http_build_query($params)));
-        if (isset($_GET['code'])) {
-            
-        $params = array(
-            'client_id' => $client_id,
-            'client_secret' => $client_secret,
-            'code' => $_GET['code'],
-            'redirect_uri' => $redirect_uri
-        );
-
-        $token = json_decode(file_get_contents('https://oauth.vk.com/access_token' . '?' . urldecode(http_build_query($params))), true);
-
-        if (isset($token['access_token'])) 
+        
+        if (isset($_GET['code'])) 
         {
             $params = array(
-                'user_ids'         => $token['user_id'],
-                'fields'       => 'id,first_name,last_name,screen_name,sex,bdate,photo_big',
-                'access_token' => $token['access_token'],
-                'v' => '5.71',
-                'name_case' => 'Nom'
+                'client_id' => $client_id,
+                'client_secret' => $client_secret,
+                'code' => $_GET['code'],
+                'redirect_uri' => $redirect_uri
             );
 
-            $userInfo = json_decode(file_get_contents('https://api.vk.com/method/users.get' . '?' . urldecode(http_build_query($params))), true);
-            print_r($userInfo);
+            $token = json_decode(file_get_contents('https://oauth.vk.com/access_token' . '?' . urldecode(http_build_query($params))), true);
 
-            if (isset($userInfo['response'][0]['id'])) 
+            if (isset($token['access_token'])) 
             {
-                $userInfo = $userInfo['response'][0];
+                $params = array(
+                    'user_ids'         => $token['user_id'],
+                    'fields'       => 'id,first_name,last_name,screen_name,sex,bdate,photo_big',
+                    'access_token' => $token['access_token'],
+                    'v' => '5.71',
+                    'name_case' => 'Nom'
+                );
 
-                session_start();
-                $_SESSION['user_info'] = $userInfo;
+                $userInfo = json_decode(file_get_contents('https://api.vk.com/method/users.get' . '?' . urldecode(http_build_query($params))), true);
+                print_r($userInfo);
+
+                if (isset($userInfo['response'][0]['id'])) 
+                {
+                    $userInfo = $userInfo['response'][0];
+
+                    session_start();
+                    $_SESSION['user_info'] = $userInfo;
+                }
             }
+        }
+        else 
+        {   
+            $params = array(
+            'client_id'     => $client_id,
+            'redirect_uri'  => $redirect_uri,
+            'response_type' => 'code'
+            );
+        
+             header("location: " . $url . '?' . urldecode(http_build_query($params)));
         }
     }
     else 
